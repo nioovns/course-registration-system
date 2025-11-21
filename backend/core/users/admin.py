7f8b9b25cd8ba289c1c2e518884c8e28ac545091
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-21 16:15:55 +0330

    Configure Django Admin for User

--- backend/core/users/admin.py
+++ backend/core/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # ستون‌ها در لیست کاربران
+    list_display = ('username', 'email', 'role', 'is_staff')
+
+    # فیلتر کردن بر اساس نقش
+    list_filter = ('role', 'is_staff')
+
+    # اضافه کردن فیلد role به صفحه ویرایش کاربر
+    fieldsets = UserAdmin.fieldsets + (
+        ('اطلاعات تکمیلی', {'fields': ('role', 'student_id', 'professor_code')}),
+    )

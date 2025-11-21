3a48e5994a0a2d2bd4527d246abc4cbcedbcee1e
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-21 17:08:14 +0330

    Implement Course model structure and test it in http://127.0.0.1:8000/admin/courses/course/add/

--- backend/core/courses/admin.py
+++ backend/core/courses/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Course
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    # ستون‌های نمایش داده شده در لیست
+    list_display = ('course_code', 'name', 'professor', 'capacity', 'schedule', 'location')
+
+    # فیلد جستجو
+    search_fields = ('name', 'course_code')
+
+    # فیلترهای سمت راست
+    list_filter = ('professor',)
+
+    # رابط کاربری بهتر برای انتخاب پیش‌نیازها
+    filter_horizontal = ('prerequisites',)
--- backend/core/courses/models.py
+++ backend/core/courses/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Course(models.Model):
+    # نام درس
+    name = models.CharField(max_length=100)
+
+    # کد درس -  یکتا
+    course_code = models.CharField(max_length=20, unique=True)
+
+    # ظرفیت کلاس - فقط اعداد مثب
+    capacity = models.PositiveIntegerField()
+
+    # استاد درس - ارتباط با جدول کاربران
+    # فقط کاربرانی که نقش 'professor' دارند قابل انتخاب هستند
+    professor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='courses_taught',
+        limit_choices_to={'role': 'professor'}
+    )
+
+    # زمان برگزاری (مثال: Saturday 14:00-16:00)
+    schedule = models.CharField(max_length=100)
+
+    # مکان برگزاری (مثال: Room 301)
+    location = models.CharField(max_length=50)
+
+    # پیش‌نیازها - ارتباط چند به چند با خودِ درس
+    # blank=True یعنی ممکن است درسی پیش‌نیاز نداشته باشد
+    prerequisites = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        blank=True,
+        related_name='required_for'
+    )
+
+    # زمان ایجاد و ویرایش رکورد (جهت اطلاع سیستم)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.course_code} - {self.name}"

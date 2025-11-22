a3e23021d0fe3749a6ae02fc117f958e7ea7056d
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-11-22 17:13:17 +0330

    feat: add time validation to ClassSession and update unit test

--- backend/core/course/models/ClassSession.py
+++ backend/core/course/models/ClassSession.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from course.validators import validate_time_range
 
 class ClassSession(models.Model):
     DAY_CHOICES = [
@@ -14,6 +16,16 @@ class ClassSession(models.Model):
     start_time = models.TimeField()
     end_time = models.TimeField()
     room = models.CharField(max_length=20)
+
+    def clean(self):
+        try:
+            validate_time_range(self.start_time, self.end_time)
+        except ValidationError as e:
+            raise e
+        
+    def save(self, *args, **kwargs):
+        self.full_clean() 
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.get_day_display()} {self.start_time}-{self.end_time} | کلاس {self.room}"
--- backend/core/course/tests/test_classsession.py
+++ backend/core/course/tests/test_classsession.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from course.models.ClassSession import ClassSession
 from datetime import time
 
@@ -21,3 +22,35 @@ class ClassSessionModelTest(TestCase):
     def test_str_method(self):
         expected = "شنبه 14:00:00-16:00:00 | کلاس 301"
         self.assertEqual(str(self.session), expected)
+    
+    def test_end_time_after_start_time(self):
+        session = ClassSession(
+            day='tue',
+            start_time=time(11, 0),
+            end_time=time(10, 0),
+            room='102'
+        )
+        with self.assertRaises(ValidationError) as cm:
+            session.full_clean()
+        self.assertIn("end_time must be later than start_time", str(cm.exception))
+    
+    def test_time_within_allowed_range(self):
+        early_session = ClassSession(
+            day='wed',
+            start_time=time(7, 30),
+            end_time=time(9, 0),
+            room='103'
+        )
+        with self.assertRaises(ValidationError) as cm1:
+            early_session.full_clean()
+        self.assertIn("Class times must be between", str(cm1.exception))
+
+        late_session = ClassSession(
+            day='thu',
+            start_time=time(19, 0),
+            end_time=time(21, 0),
+            room='104'
+        )
+        with self.assertRaises(ValidationError) as cm2:
+            late_session.full_clean()
+        self.assertIn("Class times must be between", str(cm2.exception))

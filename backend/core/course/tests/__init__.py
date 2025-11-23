f3aa274cadf7f4c2d49a2e71ae7884d79cf482d0
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-11-23 21:53:51 +0330

    test: add update tests, success, time validations,capacity and unique code validation

--- backend/core/course/tests/__init__.py
+++ backend/core/course/tests/__init__.py
@@ -3,3 +3,4 @@ from .test_course import CourseModelTest
 from .test_courseSerializer import TestCourseSerializer
 from .test_read import CourseViewSetTest
 from .test_create import AdminCreate
+from .test_update import CourseUpdateTests

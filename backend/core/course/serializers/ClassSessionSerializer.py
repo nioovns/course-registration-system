f8fa18c9f797c18afa127b0efd581be596253f0f
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-11-23 21:09:29 +0330

    fix: handle time validation error in ClassSessionSerializer

--- backend/core/course/serializers/ClassSessionSerializer.py
+++ backend/core/course/serializers/ClassSessionSerializer.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
 from course.models.ClassSession import ClassSession
+from course.validators import validate_time_range
 
 class ClassSessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassSession
         fields = '__all__'
+        
+    def validate(self, data):
+        try:
+            validate_time_range(data['start_time'], data['end_time'])
+        except ValidationError as e:
+            raise serializers.ValidationError({
+                'time_range': e.message
+            })
+        return data

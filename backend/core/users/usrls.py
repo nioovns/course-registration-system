632d7903d912d9acfdb8ce45740899712fa93b41
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 20:35:22 +0330

    feat(users-urls): add professor-dashboard route (dashboard/professor/)

--- backend/core/users/usrls.py
+++ backend/core/users/usrls.py
@@ -2,3 +2,4 @@ from django.urls import path
 from .views import AdminDashboardView, ProfessorDashboardView, StudentDashboardView
 
 path('dashboard/admin/', AdminDashboardView.as_view(), name='admin-dashboard'),
+path('dashboard/professor/', ProfessorDashboardView.as_view(), name='professor-dashboard'),

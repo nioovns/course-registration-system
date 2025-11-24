27bef79bcbd1e9cd8febb2d9e4201a26143dc7e5
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 20:57:09 +0330

    chore(users-test_permissions): create test_permissions.py and imports

---
+++ backend/core/users/tests/test_permissions.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+User = get_user_model()
+

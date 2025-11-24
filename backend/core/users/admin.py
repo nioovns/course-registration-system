b5fdb75aad3ad2868ca148333ed48748a957a87d
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 17:00:26 +0330

    feat(admin): register CustomUserAdmin for custom User model
    Register admin class to customize list display and forms for the new User.

--- backend/core/users/admin.py
+++ backend/core/users/admin.py
@@ -4,3 +4,6 @@ from django.contrib import admin
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):

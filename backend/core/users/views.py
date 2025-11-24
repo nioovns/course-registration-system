343965ce69f6211911bddbe8e30e8d83d1613a3b
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 20:30:53 +0330

    feat(users-views): add AdminDashboardView protected by IsAdmin
    Simple GET endpoint returns a welcome message for admins.

--- backend/core/users/views.py
+++ backend/core/users/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .permissions import IsAdmin, IsProfessor, IsStudent
+
+class AdminDashboardView(APIView):
+    permission_classes = [IsAdmin]
+
+    def get(self, request):
+        return Response({"message": "Welcome Admin!"})
+

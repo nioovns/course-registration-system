2cb1c9d5622daa316e70d384ad705bfc0aff06da
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 21:23:13 +0330

    fix(users-urls); FIX URLS

--- backend/core/users/urls.py
+++ backend/core/users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import AdminDashboardView, ProfessorDashboardView, StudentDashboardView
+from .views import (
+    AdminDashboardView,
+    ProfessorDashboardView,
+    StudentDashboardView
+)
 
-path('dashboard/admin/', AdminDashboardView.as_view(), name='admin-dashboard'),
-path('dashboard/professor/', ProfessorDashboardView.as_view(), name='professor-dashboard'),
-path('dashboard/student/', StudentDashboardView.as_view(), name='student-dashboard'),
+urlpatterns = [
+    path('dashboard/admin/', AdminDashboardView.as_view(), name='admin-dashboard'),
+    path('dashboard/professor/', ProfessorDashboardView.as_view(), name='professor-dashboard'),
+    path('dashboard/student/', StudentDashboardView.as_view(), name='student-dashboard'),
+]

4eb492cebecfb2b67f7dc60daa54f29d2f185e71
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 15:51:49 +0330

    feat(auth): add JWT Token lifespan

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -133,3 +133,12 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+#jwt
+#Token lifespan
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}

4c876a42f18dc29dabf6ca5f032767b053ccaa6b
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 20:26:52 +0330

    feat(users/permissions): add IsStudent permission class
    Allow access only for authenticated student users.

--- backend/core/users/permissions.py
+++ backend/core/users/permissions.py
@@ -9,3 +9,6 @@ class IsProfessor(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_professor
 
+class IsStudent(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_student

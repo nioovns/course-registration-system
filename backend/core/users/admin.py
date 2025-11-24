e14502c21eed2cead473fa7abc252e820f26998a
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 18:14:38 +0330

    fix(Users) : from .models import Users

--- backend/core/users/admin.py
+++ backend/core/users/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import Users
 
-@admin.register(User)
+@admin.register(Users)
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
 

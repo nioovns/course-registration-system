0bbeb5a115063f8b95f6aa76028c79d4e3ae6af9
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 16:45:25 +0330

    feat(users): create custom User model replacing Django default
    Introduce User model inheriting AbstractUser and add module docstring.

--- backend/core/users/models.py
+++ backend/core/users/models.py
@@ -5,3 +5,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
+
+class users(AbstractUser):

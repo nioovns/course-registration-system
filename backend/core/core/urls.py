5a0ab4da8fbbdc1bf9cc218a286f9caf3ac54ed7
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 23:35:36 +0330

    fix(core-urls): some changes for bugss     path('api/auth/logout/', LogoutView.as_view(), name='auth_logout'),

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from django.urls import path, re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+#from django.contrib.auth.views import LogoutView
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+from backend.core.users.views import LogoutView , CustomTokenObtainPairView
 
 # تنظیمات سواگر (Swagger)
 schema_view = get_schema_view(
@@ -34,6 +36,9 @@ urlpatterns = [
     # 2 (JWT)
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/logout/', LogoutView.as_view(), name='auth_logout'),
+
+    #path('api/users/', include('users.urls')),
 
     # 3 documentation API (Swagger & Redoc)
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),

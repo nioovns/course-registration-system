fe369cc43f54cf7c5ca60dc0f3f3e8b9347d2c0d
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 21:44:06 +0330

    chore(users-serializers): create serializers.py and import TokenObtainPairSerializer
    Create backend/core/users/serializers.py and add import for customizing JWT serializer.

--- backend/core/users/serializers.py
+++ backend/core/users/serializers.py
@@ -1,3 +1,14 @@
 # backend/core/users/serializers.py
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        return data

a35a594b64d58b7a0d7c0a97105b510c54ccf871
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-24 20:26:21 +0330

    feat(users/permissions): add IsProfessor permission class
    Allow access only for authenticated professor users.

--- backend/core/users/permissions.py
+++ backend/core/users/permissions.py
@@ -3,3 +3,9 @@ from rest_framework.permissions import BasePermission
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_admin_role
+
+
+class IsProfessor(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_professor
+

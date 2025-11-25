5037ef80746eef0b337dbeb5eac345df54e3dfb0
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-25 20:26:49 +0330

    test(users-swagger): add test_redoc_loads_successfully

--- backend/core/users/tests/test_swagger.py
+++ backend/core/users/tests/test_swagger.py
@@ -9,3 +9,9 @@ class SwaggerTests(APITestCase):
         url = reverse('schema-swagger-ui')  # نامی که در urls.py دادیم
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_redoc_loads_successfully(self):
+
+        url = reverse('schema-redoc')  # نامی که در urls.py دادیم
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

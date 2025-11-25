233651af1a2abdeacb3c927d1c3607087cbb11ba
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-25 20:26:08 +0330

    test(users-swagger): add test_swagger_ui_loads_successfully

--- backend/core/users/tests/test_swagger.py
+++ backend/core/users/tests/test_swagger.py
@@ -3,3 +3,9 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 
 class SwaggerTests(APITestCase):
+
+    def test_swagger_ui_loads_successfully(self):
+
+        url = reverse('schema-swagger-ui')  # نامی که در urls.py دادیم
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

630651763138df29794b9a535c6cce11a5b78278
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-26 18:04:38 +0330

    test(users/test_advanced): add AdvancedAuthTests class and setUp with sample student and URLs

--- backend/core/users/tests/test_advanced.py
+++ backend/core/users/tests/test_advanced.py
@@ -17,3 +17,22 @@ class AdvancedAuthTests(APITestCase):
         )
         self.login_url = reverse('token_obtain_pair')
         self.logout_url = reverse('auth_logout')
+
+    def test_jwt_payload_contains_custom_claims(self):
+
+        # 1. لاگین
+        response = self.client.post(self.login_url, {
+            'username': 'student_adv',
+            'password': 'password123'
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # 2. استخراج و دیکود کردن توکن
+        token_str = response.data['access']
+        token = AccessToken(token_str)
+
+        # 3. بررسی اینکه آیا اطلاعات ما داخل توکن هست؟
+        self.assertEqual(token['role'], 'student')
+        self.assertEqual(token['username'], 'student_adv')
+        # student id serialiazers
+        self.assertEqual(token['student_id'], '99123456')

08233df5af2581259ac48430f6d69eae881b7e98
Farzanehhh7 <farzanehwr@gmail.com>
2025-11-26 18:01:14 +0330

    chore(users/test_advanced): create test_advanced.py and add imports

---
+++ backend/core/users/tests/test_advanced.py
@@ -0,0 +1,8 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework_simplejwt.tokens import AccessToken
+
+User = get_user_model()
+

6b27907f83a3f14eb5d6fffd6dae339e3a2a4458
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-11-27 23:49:00 +0330

    test: add test for choices

--- backend/core/course/urls.py
+++ backend/core/course/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include 
 from rest_framework.routers import DefaultRouter
 from .views import CourseViewSet
+from .services.AllChoices import AllChoices   
 
 router = DefaultRouter()
 router.register(r'admin/courses', CourseViewSet, basename='course')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('choices/', AllChoices.as_view(), name='all-choices'),
 ]

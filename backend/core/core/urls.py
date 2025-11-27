01703239fd2bbb57aee512fd8a4e8022c82bde71
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-11-28 00:08:22 +0330

    feat: add admin prefix to course and choices endpoints

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -24,9 +24,9 @@ urlpatterns = [
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/logout/', LogoutView.as_view(), name='auth_logout'),
 
-    # 2. Users and courses App Routes
+    # 2. Users and App Routes
     path('api/users/', include('users.urls')),
-    path('api/', include('course.urls')),
+    path('api/admin/', include('course.urls')),
 
     # 3. Swagger
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),

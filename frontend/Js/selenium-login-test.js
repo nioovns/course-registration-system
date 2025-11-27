692c1813dc995791cf5b8d4b1659df0954bbb48b
elmira-orooji <elmiraorooji78@gmail.com>
2025-11-28 00:12:47 +0330

    feat : Selenium/WebDriver builder

--- frontend/Js/selenium-login-test.js
+++ frontend/Js/selenium-login-test.js
@@ -2,3 +2,13 @@ const { Builder, By, until } = require("selenium-webdriver");
 const LOGIN_URL = "http://127.0.0.1:5500/Pages/login.html";
 
 const DEFAULT_TIMEOUT = 8000;
+
+async function buildDriver() {
+  const driver = await new Builder().forBrowser("chrome").build();
+  await driver.manage().setTimeouts({
+    implicit: 0,
+    pageLoad: 10000,
+    script: 10000,
+  });
+  return driver;
+}

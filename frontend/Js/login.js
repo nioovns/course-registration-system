e39a0de12d1a109deac751c4c621da0c0b3d4634
elmira-orooji <elmiraorooji78@gmail.com>
2025-11-27 19:05:46 +0330

    feat : Js/FieldContainer + Input func

--- frontend/Js/login.js
+++ frontend/Js/login.js
@@ -1,1 +1,29 @@
+const $ = (s) => document.querySelector(s);
 
+document.addEventListener("DOMContentLoaded", () => {
+  const userField = document.querySelector(".user-name");
+  const passField = document.querySelector(".pass-word");
+  const captchaField = document.querySelector(".capcha");
+
+  const usernameContainer = document.querySelector(".user-name .frame-24");
+  const passwordContainer = document.querySelector(".pass-word .frame-24");
+  const captchaInputContainer = document.querySelector(".capcha .frame-4");
+  const captchaVisualContainer = document.querySelector(".capcha .frame-5");
+
+  const loginBtn = document.querySelector(".login-submit");
+  const refreshBtn = document.querySelector(".mdi-light-refresh");
+  const rememberBox = document.querySelector(".remember-me .frame-6");
+
+  let currentCaptcha = "";
+  let rememberMeState = false;
+
+ 
+  function createTextInput(id, type, placeholder) {
+    const input = document.createElement("input");
+    input.id = id;
+    input.type = type;
+    input.placeholder = placeholder;
+    input.className = "text-input";
+    input.dir = "rtl";
+    return input;
+  }

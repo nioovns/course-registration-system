2c3b60a2f090b7761653a357c61d69a923c2a01d
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-11-28 19:32:57 +0330

    feat: add CourseFilters for course search by course-code only

--- backend/core/course/views.py
+++ backend/core/course/views.py
@@ -5,14 +5,16 @@ from course.serializers.ClassSessionSerializer import ClassSessionSerializer
 from course.services.AdminServices import AdminService
 from course.models.Course import Course
 from users.permissions import IsAdmin
+from course.services.CourseFilters import CourseFilter
 
 class CourseViewSet(viewsets.ViewSet):
     # permission_classes= [IsAdmin]
     service = AdminService()
     
     def list(self, request):
-        courses = self.service.list_courses()
-        serializer = CourseSerializer(courses, many=True)
+        queryset = self.service.list_courses()
+        filtered = CourseFilter.apply(queryset, request.query_params)
+        serializer = CourseSerializer(filtered, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):

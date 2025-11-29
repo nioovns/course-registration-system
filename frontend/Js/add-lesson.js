36a9f3a685f90e3891a26cd624d7e5d95a0c3ef8
elmira-orooji <elmiraorooji78@gmail.com>
2025-11-30 00:42:12 +0330

    feat : Js/init main elements

---
+++ frontend/Js/add-lesson.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const $ = (s) => document.querySelector(s);
+
+  const saveBtn = document.querySelector(".group-98 .login-submit");
+  const cancelBtn = document.querySelector(".group-98 .login-submit2");
+
+  const nameEl = document.querySelector(".group-65 ._1"); 
+  const codeEl = document.querySelector(".group-66 ._493284"); 
+  const capacityEl = document.querySelector(".group-67 ._30"); 
+  const teacherEl = document.querySelector(".group-652 .p-name"); 
+  const searchEl = document.querySelector(".th4 .search"); 
+
+  const unitsWrapper = document.querySelector(".group-68 .frame-34");
+  const unitsTextEl = document.querySelector(".group-68 ._3");

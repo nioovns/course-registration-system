4968cbda1c59004b80b8578501a4436f15916e4b
elmira-orooji <elmiraorooji78@gmail.com>
2025-11-29 16:15:51 +0330

    feat : Selenium/Routing

--- frontend/Js/selenium-admin-dashboard-list-test.js
+++ frontend/Js/selenium-admin-dashboard-list-test.js
@@ -1,1 +1,7 @@
+const { Builder, By, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+
+const DASHBOARD_URL = "http://127.0.0.1:5500/Pages/admin-dashboard-list.html";
+const LOGIN_URL_FRAGMENT = "login.html";
+const ADD_LESSON_URL_FRAGMENT = "add-lesson.html";
 

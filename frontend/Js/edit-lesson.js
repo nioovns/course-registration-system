d1abae36a40d20031a997d3e70a7e86049b8a111
elmira-orooji <elmiraorooji78@gmail.com>
2025-12-01 02:53:01 +0330

    feat : Js/Dashboard date & time

--- frontend/Js/edit-lesson.js
+++ frontend/Js/edit-lesson.js
@@ -837,3 +837,37 @@ function getText(el) {
   }
 
 });
+
+(function () {
+  function updateDateTime() {
+    const now = new Date();
+
+    const persianDate = new Intl.DateTimeFormat("fa-IR", {
+      day: "numeric",
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      
+    }).format(now);
+
+    const persianTime = new Intl.DateTimeFormat("fa-IR", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    }).format(now);
+
+    
+    const dateTarget = document.querySelector("._1-1404");
+
+    if (!dateTarget) {
+      
+      console.warn("Element with class _1-1404 not found");
+      return;
+    }
+
+    dateTarget.textContent = `${persianTime} | ${persianDate}`;
+  }
+
+  updateDateTime();
+  setInterval(updateDateTime, 1000);
+})();

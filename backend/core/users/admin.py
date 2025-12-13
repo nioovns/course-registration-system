03e42858ad061a74fafe8a040381f3509c5edb29
Farzanehhh7 <farzanehwr@gmail.com>
2025-12-14 02:45:37 +0330

    feat(users-admin): add stacked inlines for student and professor profiles

--- backend/core/users/admin.py
+++ backend/core/users/admin.py
@@ -1,25 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Student, Professor
 
-# Register your models here.
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+
+class StudentInline(admin.StackedInline):
+    model = Student
+    can_delete = False
+    verbose_name_plural = 'Student Profile'
+    fk_name = 'user'
+
+
+class ProfessorInline(admin.StackedInline):
+    model = Professor
+    can_delete = False
+    verbose_name_plural = 'Professor Profile'
+    fk_name = 'user'
+
 
 @admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
-
-    list_filter = ('role', 'is_staff', 'is_superuser', 'is_active')
-    search_fields = ('username', 'first_name', 'last_name', 'email', 'student_id', 'professor_code')
-
-    fieldsets = UserAdmin.fieldsets + (
-        ('اطلاعات تکمیلی دانشگاه', {
-            'fields': ('role', 'student_id', 'professor_code'),
-        }),
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None, {'fields': ('role',)}),
     )
 
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('اطلاعات تکمیلی دانشگاه', {
-            'fields': ('role', 'student_id', 'professor_code'),
-        }),
-    )
+    inlines = (StudentInline, ProfessorInline)
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_id', 'first_name', 'last_name', 'user')
+
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('professor_code', 'first_name', 'last_name', 'user')

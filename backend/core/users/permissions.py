85fd94aa3727cf81e9f458f32dd5d455aa99812b
Farzanehhh7 <farzanehwr@gmail.com>
2025-12-14 02:49:37 +0330

    refactor(users-views): implement role-based dashboard logic and cleanup unused views

--- backend/core/users/permissions.py
+++ backend/core/users/permissions.py
@@ -1,8 +1,7 @@
-from rest_framework.permissions import BasePermission , SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-
         return bool(request.user and request.user.is_authenticated and request.user.is_admin_role)
 
 class IsProfessor(BasePermission):

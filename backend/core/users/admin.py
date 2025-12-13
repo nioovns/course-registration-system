570090d590501c6d6c0a3c36fae874ba43cb9a9e
Farzanehhh7 <farzanehwr@gmail.com>
2025-12-14 03:21:35 +0330

    feat(admin-ux): implement unified creation form for students and professors with auto-user generation

--- backend/core/users/admin.py
+++ backend/core/users/admin.py
@@ -1,41 +1,51 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Student, Professor
-
-
-class StudentInline(admin.StackedInline):
-    model = Student
-    can_delete = False
-    verbose_name_plural = 'Student Profile'
-    fk_name = 'user'
-
-
-class ProfessorInline(admin.StackedInline):
-    model = Professor
-    can_delete = False
-    verbose_name_plural = 'Professor Profile'
-    fk_name = 'user'
-
-
-@admin.register(User)
-class CustomUserAdmin(BaseUserAdmin):
-    list_display = ('username', 'email', 'role', 'is_staff')
-    list_filter = ('role', 'is_staff', 'is_superuser')
-    fieldsets = BaseUserAdmin.fieldsets + (
-        (None, {'fields': ('role',)}),
-    )
-    add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        (None, {'fields': ('role',)}),
-    )
-
-    inlines = (StudentInline, ProfessorInline)
-
-
+from .forms import StudentCreationForm, ProfessorCreationForm
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('student_id', 'first_name', 'last_name', 'entry_year', 'user')
+    list_display = ('student_id', 'first_name', 'last_name', 'entry_year', 'get_username')
+    search_fields = ('student_id', 'first_name', 'last_name')
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    get_username.short_description = 'Username'
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj is None:
+            kwargs['form'] = StudentCreationForm
+        return super().get_form(request, obj, **kwargs)
+
+    # مخفی کردن فیلد user در صفحه ویرایش (چون خودکار پر شده)
+    def get_exclude(self, request, obj=None):
+        if obj is None:
+            return ['user']
+        return []
 
 
 @admin.register(Professor)
 class ProfessorAdmin(admin.ModelAdmin):
-    list_display = ('professor_code', 'first_name', 'last_name', 'user')
+    list_display = ('professor_code', 'first_name', 'last_name', 'get_username')
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    get_username.short_description = 'Username'
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj is None:
+            kwargs['form'] = ProfessorCreationForm
+        return super().get_form(request, obj, **kwargs)
+
+    def get_exclude(self, request, obj=None):
+        if obj is None:
+            return ['user']
+        return []
+
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ('username', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff')
+    # inlines = (StudentInline, ProfessorInline)

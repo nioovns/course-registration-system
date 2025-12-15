830cbb7916d315246cf3da72d3fe4680b119d595
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-12-15 15:48:21 +0330

    feat: enrollmentSetting serializer added

---
+++ backend/core/enrollment/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import EnrollmentSettings
+
+class EnrollmentSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EnrollmentSettings
+        fields = "__all__"
+
+    def validate(self, attrs):
+        min_units = attrs.get("min_units")
+        max_units = attrs.get("max_units")
+
+        if min_units >= max_units:
+            raise serializers.ValidationError(
+                "حداقل واحد باید کمتر از حداکثر واحد باشد"
+            )
+
+        return attrs

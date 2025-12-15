a71c7ac8514eeef8dc500b6453a030c1b5565e40
Farzanehhh7 <farzanehwr@gmail.com>
2025-12-15 21:30:57 +0330

    fix(swagger): configure JWT Bearer authentication settings for API documentation

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -14,7 +14,7 @@ schema_view = get_schema_view(
       description="API Documentation",
    ),
    public=True,
-   permission_classes=(permissions.AllowAny,),
+   permission_classes=[permissions.AllowAny,],
 )
 
 urlpatterns = [

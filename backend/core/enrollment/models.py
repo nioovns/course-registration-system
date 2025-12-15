f933b901240e2d93dfcbcad3e68698af20b4e160
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-12-15 15:36:58 +0330

    feat: create enrollmentSetting model

--- backend/core/enrollment/models.py
+++ backend/core/enrollment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class EnrollmentSettings(models.Model):
+    min_units = models.PositiveIntegerField()
+    max_units = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.min_units} - {self.max_units}"
+    

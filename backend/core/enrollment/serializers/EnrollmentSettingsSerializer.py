48c817ba3f5aabd543026604d9ee06452ab8b6d9
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-12-28 01:52:11 +0330

    feat: add validation for enrollment-setting (not null - max bigger than min - positive numbers - max limit is 50)

--- backend/core/enrollment/serializers/EnrollmentSettingsSerializer.py
+++ backend/core/enrollment/serializers/EnrollmentSettingsSerializer.py
@@ -12,9 +12,21 @@ class EnrollmentSettingsSerializer(serializers.ModelSerializer):
         min_units = attrs.get("min_units")
         max_units = attrs.get("max_units")
 
+        if min_units is None or max_units is None:
+            raise serializers.ValidationError("حداقل و حداکثر واحد نمی‌توانند خالی باشند")
+        
         if min_units >= max_units:
             raise serializers.ValidationError(
                 "حداقل واحد باید کمتر از حداکثر واحد باشد"
             )
 
+        if min_units <= 0 or max_units <= 0:
+            raise serializers.ValidationError(
+                "واحدها باید عددی مثبت باشند"
+            )
+            
+        MAX_LIMIT = 50
+        if max_units > MAX_LIMIT:
+            raise serializers.ValidationError(f"حداکثر واحد نمی‌تواند بیشتر از {MAX_LIMIT} باشد")
+        
         return attrs

e69a7119cf638339513ff3300638566a9d3685d2
Farzanehhh7 <farzanehwr@gmail.com>
2025-12-27 17:36:57 +0330

    refactor(course): link professor field to Professor model instead of User

--- backend/core/course/models/ClassSession.py
+++ backend/core/course/models/ClassSession.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-# اگر ایمپورت‌های زیر ارور داد، از ..validators استفاده کنید
 from course.validators import validate_time_range
-from course.choices import TimeChoices, FacultyChoices, RoomChoices, DayChoices, UnitChoices
+from course.choices import FacultyChoices, UnitChoices, DayChoices
+# 1. ایمپورت صحیح مدل استاد از اپ users
+from users.models import Professor
 
 
 class ClassSession(models.Model):
@@ -11,21 +12,61 @@ class ClassSession(models.Model):
     end_time = models.TimeField()
     faculty = models.CharField(max_length=20, choices=FacultyChoices.FACULTY_CHOICES, default="eng")
     room = models.CharField(max_length=20)
-
     class Meta:
-        app_label = 'course'  #
+        app_label = 'course'
         verbose_name = 'Class Session'
         verbose_name_plural = 'Class Sessions'
 
     def clean(self):
-        try:
-            validate_time_range(self.start_time, self.end_time)
-        except ValidationError as e:
-            raise e
+        validate_time_range(self.start_time, self.end_time)
 
     def save(self, *args, **kwargs):
         self.full_clean()
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.get_day_display()} {self.start_time}-{self.end_time} | کلاس {self.room}"
+        return f"{self.get_day_display()} {self.start_time.strftime('%H:%M')}-{self.end_time.strftime('%H:%M')} | Room {self.room}"
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20)
+    units = models.IntegerField(choices=UnitChoices.UNIT_CHOICES, default=1)
+
+    capacity = models.PositiveIntegerField()
+
+    sessions = models.ManyToManyField(ClassSession, related_name="courses")
+
+    professor = models.ForeignKey(
+        Professor,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='courses'
+    )
+
+    prerequisites = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        blank=True,
+        related_name="required_for"
+    )
+
+    class Meta:
+        app_label = 'course'
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+
+    def __str__(self):
+        prof_name = self.professor.last_name if self.professor else "No Prof"
+        return f"{self.name} ({self.code}) - {prof_name}"
+
+    @property
+    def is_full(self):
+        if hasattr(self, 'enrollments'):
+            return self.enrollments.count() >= self.capacity
+        return False
+
+    def delete(self, *args, **kwargs):
+        self.sessions.all().delete()
+        super().delete(*args, **kwargs)

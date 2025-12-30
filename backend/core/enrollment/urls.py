b656ba0bc60bd259a0039ff4a6ad146610374510
Farzanehhh7 <farzanehwr@gmail.com>
2025-12-31 02:59:38 +0330

    feat(enrollment): combine EnrollmentSettings (Admin) and StudentEnrollment views

--- backend/core/enrollment/urls.py
+++ backend/core/enrollment/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EnrollmentSettingsViewSet
+from .views import EnrollmentSettingsViewSet, EnrollmentViewSet
 
 router = DefaultRouter()
 router.register(r'enrollment-settings', EnrollmentSettingsViewSet)
+router.register(r'settings', EnrollmentSettingsViewSet, basename='enrollment-settings')
 
+# ۲. آدرس انتخاب واحد (دانشجو) -> /api/enrollment/my-courses/
+router.register(r'my-courses', EnrollmentViewSet, basename='enrollment')
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- backend/core/enrollment/views.py
+++ backend/core/enrollment/views.py
@@ -1,9 +1,22 @@
-from rest_framework import viewsets
-from enrollment.models.EnrollmentSettings import EnrollmentSettings
-from enrollment.serializers.EnrollmentSettingsSerializer import EnrollmentSettingsSerializer
-from users.permissions import IsAdmin
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from users.permissions import IsAdmin, IsStudent
+from .models import  EnrollmentSettings
+from .serializers import  EnrollmentSettingsSerializer
+from .serializers.EnrollmentSettingsSerializer import EnrollmentSerializer
 
 class EnrollmentSettingsViewSet(viewsets.ModelViewSet):
     queryset = EnrollmentSettings.objects.all()
     serializer_class = EnrollmentSettingsSerializer
     permission_classes = [IsAdmin]
+
+class EnrollmentViewSet(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        mixins.DestroyModelMixin,
+                        viewsets.GenericViewSet):
+
+    serializer_class = EnrollmentSerializer
+    permission_classes = [IsAuthenticated, IsStudent]
+
+    def get_queryset(self):
+        return EnrollmentSerializer.objects.filter(student=self.request.user.student).select_related('course', 'course__professor')

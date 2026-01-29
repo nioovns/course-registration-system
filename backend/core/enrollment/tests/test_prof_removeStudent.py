d133dbd1c2699cb7c09f965d03d024870fdbbd62
nvns <nioshavarnaseri@gmail.com>
2026-01-30 01:28:53 +0330

    test: add service and view test for professor remove student

---
+++ backend/core/enrollment/tests/test_prof_removeStudent.py
@@ -0,0 +1,52 @@
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from enrollment.services import professor_remove_student
+from enrollment.models import Enrollment
+from users.models import User, Professor, Student
+from course.models.Course import Course
+
+class ProfessorRemoveStudentServiceTest(TestCase):
+
+    def setUp(self):
+        self.prof_user1 = User.objects.create_user(username='prof1', password='pass123', role= User.Roles.PROFESSOR)
+        self.prof1 = Professor.objects.create(user=self.prof_user1, professor_code= "123")
+        self.student_user = User.objects.create_user(username='student1', password='pass123', role= User.Roles.STUDENT)
+        self.student = Student.objects.create(user=self.student_user)
+        self.course = Course.objects.create(
+            name="Math 101",
+            code="MATH101",
+            capacity=30,
+            professor=self.prof_user1
+        )
+        self.enrollment = Enrollment.objects.create(
+            student=self.student,
+            course=self.course,
+            status=Enrollment.Status.ENROLLED
+        )
+
+    def test_professor_can_remove_student(self):
+        old_capacity = self.course.capacity
+
+        professor_remove_student(
+            professor=self.prof1,
+            course_id=self.course.id,
+            student_id=self.student.id
+        )
+
+        self.course.refresh_from_db()
+        self.assertEqual(self.course.capacity, old_capacity + 1)
+        self.assertFalse(
+            Enrollment.objects.filter(id=self.enrollment.id).exists()
+        )
+
+    def test_other_professor_cannot_remove_student(self):
+        self.prof_user2 = User.objects.create_user(username='prof2', password='pass123', role= User.Roles.PROFESSOR)
+        other_professor = Professor.objects.create(user=self.prof_user2, professor_code= "1234")
+
+        with self.assertRaises(ValidationError):
+            professor_remove_student(
+                professor=other_professor,
+                course_id=self.course.id,
+                student_id=self.student.id
+            )
+
